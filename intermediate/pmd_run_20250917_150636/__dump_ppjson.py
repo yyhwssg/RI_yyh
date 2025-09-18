df9031b39bc4effa5f77a7f19bc023bb237a228d
yyhwssg <95628889+yyhwssg@users.noreply.github.com>
2025-09-18 11:46:19 +0800

    chore: add .gitignore and untrack venv/outputs/caches

--- intermediate/pmd_run_20250917_150636/__dump_ppjson.py
+++
@@ -1,6 +0,0 @@
-﻿import pandapower as pp, json
-from pandapower.file_io import PPJSONEncoder
-net = pp.from_pickle(r'E:\PF_CLEAN\saved_json\grids\3b86aa918529\net.p')
-with open(r'E:\PF_CLEAN\intermediate\pmd_run_20250917_150636\net_power.json',"w",encoding="utf-8") as f:
-    json.dump(net, f, cls=PPJSONEncoder, indent=2, ensure_ascii=False)
-print(r'E:\PF_CLEAN\intermediate\pmd_run_20250917_150636\net_power.json')
